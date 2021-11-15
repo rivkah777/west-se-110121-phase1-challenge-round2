de12d7623c9d6f485aedeaf1efbc84403179947a
Rebekah <rbk1008@Gmail.com>
2021-11-15 16:13:39 -0700

    hope it pass

--- src/index.js
+++ src/index.js
@@ -8,22 +8,50 @@ fetch (API)
 
 
 function flata(flataGram){
-    document.querySelector("#fg-title").textContent = flataGram.title;
-    document.querySelector("#fg-image").src = flataGram.image;
-    document.getElementById("fg-likes").textContent = `${flataGram.likes} likes`;
+    document.querySelector("#fg-title").textContent = flataGram.title; //image title
+    document.querySelector("#fg-image").src = flataGram.image; //image card 
     
+   
+    document.getElementById("like-button").addEventListener("click", cb);//
+      
+    document.getElementById("fg-likes").textContent = '0 Likes';// likes counter
+    likesCounter = 0;
     
-
-    const comentarios = document.querySelector("#fg-comments");
-   comentarios.innerHTML = "";
+        
+    function cb(){
+        ++ likesCounter;
+        document.getElementById("fg-likes").textContent = `${likesCounter} Likes`;
+    }
+   
+   
+   
+    const comentarios = document.querySelector("#fg-comments");//select comments id 
+   comentarios.innerHTML = ""; // remove old comments
     
-   flataGram.comments.forEach((comments) => {
-    const li = document.createElement("li");
+   flataGram.comments.forEach((comments) => { //go thrue each line 
+    const li = document.createElement("li");//creating a new elemnt list
     li.textContent = comments.content
-    comentarios.append(li);
+    comentarios.append(li);// add new comments to the DOM
 
    })
 
-  
+ } 
+   
 
-}
+    let formaDeComentarios = document.getElementById("comment-form");// select the id 
+    formaDeComentarios.addEventListener("submit", agregarComentarios);//add comment
+
+
+    const comentarios = document.querySelector("#fg-comments");
+
+     function agregarComentarios(event){
+        event.preventDefault();
+        let commentText = event.target.comment.value;
+        
+        let comentario = document.createElement("li");// crater a list element
+        comentario.textContent = commentText;// add text comments
+        comentarios.appendChild(comentario);
+        formaDeComentarios.reset();// remove what ever you write on the search bar
+   
+                
+     }
