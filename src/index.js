e44a5a708241cc7e37b8a5796b5baf4ef0d321fa
Rebekah <rbk1008@Gmail.com>
2021-11-15 12:03:53 -0700

    Done with assigment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("you got this!");
+const API = ("https://distinct-vaulted-freesia.glitch.me/image")
+
+
+
+fetch (API)
+.then((res) => res.json())
+.then(flata)
+
+
+function flata(flataGram){
+    document.querySelector("#fg-title").textContent = flataGram.title;
+    document.querySelector("#fg-image").src = flataGram.image;
+    document.getElementById("fg-likes").textContent = `${flataGram.likes} likes`;
+    
+    
+
+    const comentarios = document.querySelector("#fg-comments");
+   comentarios.innerHTML = "";
+    
+   flataGram.comments.forEach((comments) => {
+    const li = document.createElement("li");
+    li.textContent = comments.content
+    comentarios.append(li);
+
+   })
+
+  
+
+}
